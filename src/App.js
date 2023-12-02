4964de5dba835c5a232b1dbeea9436292e818821
AutanMama <zulkiflugarba1@gmail.com>
2023-12-03 00:07:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+  const [taskList, setTaskList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTask = (taskText) => {
+    if (taskText.trim() !== "") {
+      const newTask = {
+        taskId: Math.random(),
+        taskText,
+      };
+      setTaskList([...taskList, newTask]);
+      setInput("");
+    }
+  };
+
+  const deleteTask = (taskId) => {
+    const newTaskList = taskList.filter((task) => task.taskId !== taskId);
+    setTaskList(newTaskList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="big-phone">
+        <h1>To Do List</h1>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="add-btn" onClick={() => addTask(input)}>
+          +
+        </button>
+
+        <div className="small-phone">
+          <ol className="task-list">
+            {taskList.map((task) => (
+              <li className="task-item" key={task.taskId}>
+                {task.taskText}
+                <button
+                  className="delete-btn"
+                  onClick={() => deleteTask(task.taskId)}
+                >
+                  -
+                </button>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    </>
   );
 }
 
